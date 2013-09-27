97b0c772b0175155403f14ea6ade549a4fc66903
optimisticcoder <danpatching@gmail.com>
2013-09-27 09:58:22 +0100

    Updated assembly info.

--- uApe.BackOfficeWiki/Properties/AssemblyInfo.cs
+++ uApe.BackOfficeWiki/Properties/AssemblyInfo.cs
@@ -2,34 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("uApe.BackOfficeWiki")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Wiki package for the Umbraco back office.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("uApe.BackOfficeWiki")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("uApe Ecommerce Limited")]
+[assembly: AssemblyProduct("uApe BackOffice Wiki")]
+[assembly: AssemblyCopyright("Copyright © uApe Ecommerce Limited 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4210e508-767f-4d8f-836c-d88ee6f66c48")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
